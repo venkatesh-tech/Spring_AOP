9becbcdcd80c723985e30803042e71fc711b5d3d
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-17 18:36:04 +0530

    Updated @Pointcut

--- src/main/java/com/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -4,14 +4,28 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class CommonJoinPointConfig {
 
-	@Pointcut("execution(* com.spring.aop.springaop.data.*.*(..))")
+	@Pointcut("execution(* com.spring.aop.springaop.data.*.*(..))") // with these pointcut data can be added
 	public void dataLayerExecution() {
-
 	}
 
-	@Pointcut("execution(* com.spring.aop.springaop.business.*.*(..))")
+	@Pointcut("execution(* com.spring.aop.springaop.business.*.*(..))") // with these pointcut business can be added
 	public void businessLayerExecution() { // use copy qualified name from mouse right click
+	}
 
+	@Pointcut("execution(* com.spring.aop.springaop.business.*.*(..)) && execution(* com.spring.aop.springaop.business.*.*(..))")
+	public void allLayerExecution() { // with these pointcut both business and data can be added
+	}
+
+	@Pointcut("bean(*dao*)") // Looks for any bean that contains name dao
+	public void beanStartingWithDAO() {
+	}
+
+	@Pointcut("bean(dao*)") // Looks for any bean that contains name starts with dao
+	public void beanconatiningWithDAO() {
+	}
+
+	@Pointcut("within(com.spring.aop.springaop.data.*.*(..))") // Looks for bean that contains in that package"
+	public void beanWithinDAO() {
 	}
 
 }
