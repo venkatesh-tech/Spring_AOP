1f892b4844ffd9fd8bcfeecf7c3dc287f5fbb188
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-17 11:27:24 +0530

    Added Business logic

---
+++ src/main/java/com/spring/aop/springaop/Business2.java
@@ -0,0 +1,17 @@
+package com.spring.aop.springaop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Business2 {
+
+	@Autowired
+	private Dao2 dao2;
+
+	public String calculateSomething() {
+		// Business Logic
+		return dao2.retrieveSomething();
+
+	}
+}
---
+++ src/main/java/com/spring/aop/springaop/Dao1.java
@@ -0,0 +1,11 @@
+package com.spring.aop.springaop;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao1 {
+
+	public String retrieveSomething() {
+		return "Dao1";
+	}
+}
