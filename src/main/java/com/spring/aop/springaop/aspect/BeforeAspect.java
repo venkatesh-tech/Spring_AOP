d95577733ba55d02790289426036c910f1dd8bc2
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-17 14:11:30 +0530

    Added BeforeAspect

--- src/main/java/com/spring/aop/springaop/aspect/BeforeAspect.java
+++ src/main/java/com/spring/aop/springaop/aspect/BeforeAspect.java
@@ -1,13 +1,19 @@
 package com.spring.aop.springaop.aspect;
 
+import org.slf4j.*;
+import org.springframework.context.annotation.Configuration;
+
 //AOP
 //Configuration
+@Configuration
 public class BeforeAspect {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	// What kinds of method calls I would intercept
 	public void before() {
 		// What to do?
-
+		logger.info("Intercepted Methd Calls {}");
 	}
 
 }
